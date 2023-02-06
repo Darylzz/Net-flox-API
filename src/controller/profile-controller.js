92b9aa8b62fb58b03cddf700db649c008f092914
Ingkarat boonruang <ingkaratboonruang@Ingkarats-MacBook-Air.local>
2023-02-07 00:41:35 +0700

    finish delete profile backend

--- src/controller/profile-controller.js
+++ src/controller/profile-controller.js
@@ -1,71 +1,72 @@
-const fs = require("fs")
-const path = require('path')
-const {validateCreateProfile} = require("../validator/profile-validate")
-const createError = require("../util/createError")
-const storage = require("../middlewares/upload")
-const {Profile} = require("../models")
+const fs = require("fs");
+const path = require("path");
+const { validateCreateProfile } = require("../validator/profile-validate");
+const createError = require("../util/createError");
+const storage = require("../middlewares/upload");
+const { Profile } = require("../models");
 exports.createProfile = async (req, res, next) => {
-    try {
-        
-        const value = validateCreateProfile({
-            title: req.body.title,
-            image: req.file?.path
-        })
-        
-        value.userId = req.user.id
-        
-        const profile = await Profile.create({
-            profileName: value.title,
-            profileImage: value.image,
-            userId: value.userId
-        })
-        res.status(201).json({profile})
-    }catch(err) {
-        next(err)
-    }
-}
+  try {
+    const value = validateCreateProfile({
+      title: req.body.title,
+      image: req.file?.path,
+    });
+
+    value.userId = req.user.id;
+
+    const profile = await Profile.create({
+      profileName: value.title,
+      profileImage: value.image,
+      userId: value.userId,
+    });
+    res.status(201).json({ profile });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.getAllProfileIncludeUser = async (req, res, next) => {
-    try {
-        const profile = await Profile.findAll({
-            where: {
-                userId: req.user.id
-            }
-        })
-        res.status(200).json({ profile })
-    }catch(err) {
-        next(err)
-    }
-}
+  try {
+    const profile = await Profile.findAll({
+      where: {
+        userId: req.user.id,
+      },
+    });
+    res.status(200).json({ profile });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.getProfileById = async (req, res, next) => {
-    try {
-        const profile = await Profile.findOne({
-            where: {
-                id: req.params.profileId
-            }
-        })
-        if(!profile) {
-            createError("This profile is not match", 400)
-        }
-        res.status(200).json({profile})
-    }catch(err) {
-        next(err)
+  try {
+    const profile = await Profile.findOne({
+      where: {
+        id: req.params.profileId,
+      },
+    });
+    if (!profile) {
+      createError("This profile is not match", 400);
     }
-}
+    res.status(200).json({ profile });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.deleteProfile = async (req, res, next) => {
-    try {
-        const profile = await Profile.findOne({ where: {id: req.params.profileId}})
-        if (!profile) {
-            createError("This profile not found", 400)
-        }
-        if (profile.userId !== req.user.id) {
-            createError("You have no permission delete this profile", 403)
-        }   
-        await profile.destroy()
-        res.status(204).json()
-    }catch(err) {
-        next(err)
+  try {
+    const profile = await Profile.findOne({
+      where: { id: req.params.profileId },
+    });
+    if (!profile) {
+      createError("This profile not found", 400);
     }
-}
+    if (profile.userId !== req.user.id) {
+      createError("You have no permission delete this profile", 403);
+    }
+    await profile.destroy();
+    res.status(204).json({profile});
+  } catch (err) {
+    next(err);
+  }
+};
